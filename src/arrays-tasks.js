06eec4f6ffd55b0f3ae385b075732d62a8cdb0f5
coffeekosatka <viikkuullyy@gmail.com>
2024-02-10 22:33:05 +0300

    feat: task 2

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -38,8 +38,14 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+
+  return Array.from({ length: maxLength }, (_, index) => {
+    const val1 = arr1[index] || 0;
+    const val2 = arr2[index] || 0;
+    return val1 + val2;
+  });
 }
 
 /**
