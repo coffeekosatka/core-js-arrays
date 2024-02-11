e3232d6746f30e45c8e0e1590fa37800fd615d9d
coffeekosatka <viikkuullyy@gmail.com>
2024-02-11 16:32:26 +0300

    feat: task 6 and 7

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -114,8 +114,8 @@ function removeFalsyValues(arr) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((str) => str.length);
 }
 
 /**
@@ -132,8 +132,15 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  const sum = arr.reduce((acc, value) => acc + value, 0);
+  const average = sum / arr.length;
+
+  return parseFloat(average.toFixed(2));
 }
 
 /**
