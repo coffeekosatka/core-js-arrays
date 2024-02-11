e0c932874feccc92d4101a8a5e90c5fcbd6f773b
coffeekosatka <viikkuullyy@gmail.com>
2024-02-11 16:42:26 +0300

    feat: task 8 and 9

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -153,8 +153,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const firstStringLength = arr[0].length;
+  return arr.every((str) => str.length === firstStringLength);
 }
 
 /**
@@ -168,8 +169,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((el, index) => el === index);
 }
 
 /**
