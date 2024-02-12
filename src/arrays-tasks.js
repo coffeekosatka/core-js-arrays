cd2abeb55212e30851db04068220a2cf2ae63f82
coffeekosatka <viikkuullyy@gmail.com>
2024-02-12 20:34:55 +0300

    feat: task 21

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -425,8 +425,10 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const falsyValues = arr.filter((x) => !x);
+  const falsyValuesCount = falsyValues.length;
+  return falsyValuesCount;
 }
 
 /**
